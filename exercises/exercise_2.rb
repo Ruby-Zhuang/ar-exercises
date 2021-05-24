96fcfb718fd1bef453ae3e37ec45c6a1d1d14ee8
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-24 13:34:58 -0400

    Complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Exercise 2: Update the first store
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.update(name: "Toronto")
+
+# @store1.name = "Toronto"
+# @store1.save
